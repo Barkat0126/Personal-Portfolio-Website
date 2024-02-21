a739905fa02133b8be65be2118f6f94d383869af
Barkat0126 <barkat0126@gmail.com>
2024-02-21 20:37:08 +0530

    improve about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,14 +13,19 @@ const About = () => {
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Barkat Ali, nice to meet you. Please take a look around.</p>
+              <p>Hi, I'm Barkat Ali, it's a pleasure to meet you. Please take a look around.</p>
             </div>
             <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
+              <p>⭐ I'm passionate about developing high-quality software that enhances people's lives.
+                <br />
+                ⭐ I have expertise in crafting software for a wide range of clients, from individuals to large corporations.
+                 <br />
+                ⭐ Imagine having a software expert ready to assist you at any moment what could you achieve?
+                <br />
+                ⭐ Let's explore the possibilities together—what challenges can we overcome with the right software solutions?
+                <br />
+                ⭐ Continuous learning and staying updated with the latest technologies are key aspects of my approach.
+                </p>  
             </div>
           </div>
       </div>
