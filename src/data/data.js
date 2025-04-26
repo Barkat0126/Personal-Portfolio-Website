6b5d8bd1f24225ea2f63d6df9c7186180db35bc3
Barkat0126 <barkat0126@gmail.com>
2025-04-26 21:29:54 +0530

    project 1 updated

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,15 @@
 import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
+import alpha_wave from '../assets/projects/alpha_wave.png'
 
 export const data=[
     {
         id:1,
-        name:"Project Will Be Uploaded Soon.",
+        name:"Alpha Wave Learning Custom Learning Paths, Powered by AI",
         // name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        image:alpha_wave,
+        github:"https://github.com/Barkat0126",
+        live:"https://alpha-wave-learning-web.vercel.app/",
     },
     {
         id:2,
